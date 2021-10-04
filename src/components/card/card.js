b396480d947f515c8bd29cebe5bf1daef578552d
Olga Tkach <tkacholya@gmail.com>
2021-10-04 12:00:37 -0400

    Added LinkedIn and Twitter icons and links

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { AiFillTwitterCircle } from "react-icons/ai";
 
-import './styles.css';
+import "./styles.css";
 const Card = (props) => {
-      const {image, title, githubLink} = props;
-      return(
-        <div className="post">
-          <div className="image-div"><img src={image} alt={title} /></div>
-       
-         
-            
-            <h2 className="title">{title}</h2>
-            
-            <a href={githubLink}><FaGithub  className="icons" /> </a>
-            
-        </div>
-      )
-}
+  const { image, title, githubLink, linkedinLink, twitterLink } = props;
+  return (
+    <div className="post">
+      <div className="image-div">
+        <img src={image} alt={title} />
+      </div>
+
+      <h2 className="title">{title}</h2>
+
+      <a href={githubLink}>
+        <FaGithub size={40} className="icons" />
+      </a>
+      <a href={linkedinLink}>
+        <TiSocialLinkedin size={2} className="icons" />
+      </a>
+      <a href={twitterLink}>
+        <AiFillTwitterCircle className="icons" />
+      </a>
+    </div>
+  );
+};
 
 export default Card;
